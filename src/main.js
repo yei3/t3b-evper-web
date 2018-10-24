18f62bcc1d8093d572c1d0fa1002f720fefc84cd
Esaú Peralta <esau.opr@gmail.com>
2018-10-23 22:24:08 -0500

    refactor(styles): Remove style from local scope to global require

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 import Vue from 'vue';
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
+import '@/assets/styles/general.css';
 
 import App from './App.vue';
 import router from './router';
