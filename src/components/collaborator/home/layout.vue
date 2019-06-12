63086a7d4155e47d80d21efbb333814ec035ce00
sigfried <rssanjuan2704@gmail.com>
2019-06-11 23:08:49 -0500

    [MDF] Formatting issues.

--- src/components/collaborator/home/layout.vue
+++ src/components/collaborator/home/layout.vue
@@ -1,33 +1,33 @@
 <template>
-  <div class="main-content">
-    <a-row :gutter="32" style="margin-top: 30px;">
-      <!-- <a-col :lg="6" :sm="24" style="text-align: center">
+    <div class="main-content">
+        <a-row :gutter="32" style="margin-top: 30px;">
+            <!-- <a-col :lg="6" :sm="24" style="text-align: center">
                 <br/>
                 <h2></h2>
       </a-col>-->
-      <a-col :lg="6" :sm="24"></a-col>
-      <a-col :lg="12" :sm="24">
-        <next-evaluation-period/>
-      </a-col>
+            <a-col :lg="6" :sm="24"></a-col>
+            <a-col :lg="12" :sm="24">
+                <next-evaluation-period />
+            </a-col>
 
-      <a-col :lg="6" :sm="24" style="text-align: center"></a-col>
-    </a-row>
-    <a-row style="margin-top: 30px;">
-      <a-col :span="24">
-        <current-objectives/>
-      </a-col>
-      <a-col :span="24">
-        <current-actions/>
-      </a-col>
-      <a-col :span="24">
-        <pending-auto-evaluations/>
-      </a-col>
-      <a-col :span="24">
-        <boss-evaluations/>
-      </a-col>
-    </a-row>
-    <video-modal-component ref="videoModal" :userRole="role"></video-modal-component>
-  </div>
+            <a-col :lg="6" :sm="24" style="text-align: center"></a-col>
+        </a-row>
+        <a-row style="margin-top: 30px;">
+            <a-col :span="24">
+                <current-objectives />
+            </a-col>
+            <a-col :span="24">
+                <current-actions />
+            </a-col>
+            <a-col :span="24">
+                <pending-auto-evaluations />
+            </a-col>
+            <a-col :span="24">
+                <boss-evaluations />
+            </a-col>
+        </a-row>
+        <video-modal-component ref="videoModal" :userRole="role"></video-modal-component>
+    </div>
 </template>
 
 <script>
