582bb2282bcddbf5e1c3246b624f4fa449b6ab07
sigfried <rssanjuan2704@gmail.com>
2019-06-11 23:08:01 -0500

    [MDF] Pass userRole prop to modal.

--- src/components/collaborator/home/layout.vue
+++ src/components/collaborator/home/layout.vue
@@ -1,39 +1,40 @@
 <template>
-    <div class="main-content">
-        <a-row :gutter="32" style="margin-top: 30px;">
-            <!-- <a-col :lg="6" :sm="24" style="text-align: center">
+  <div class="main-content">
+    <a-row :gutter="32" style="margin-top: 30px;">
+      <!-- <a-col :lg="6" :sm="24" style="text-align: center">
                 <br/>
                 <h2></h2>
       </a-col>-->
-            <a-col :lg="6" :sm="24"></a-col>
-            <a-col :lg="12" :sm="24">
-                <next-evaluation-period />
-            </a-col>
+      <a-col :lg="6" :sm="24"></a-col>
+      <a-col :lg="12" :sm="24">
+        <next-evaluation-period/>
+      </a-col>
 
-            <a-col :lg="6" :sm="24" style="text-align: center"></a-col>
-        </a-row>
-        <a-row style="margin-top: 30px;">
-            <a-col :span="24">
-                <current-objectives />
-            </a-col>
-            <a-col :span="24">
-                <current-actions />
-            </a-col>
-            <a-col :span="24">
-                <pending-auto-evaluations />
-            </a-col>
-            <a-col :span="24">
-                <boss-evaluations />
-            </a-col>
-        </a-row>
-        <video-modal-component ref="videoModal" userType="'collaborator'"></video-modal-component>
-    </div>
+      <a-col :lg="6" :sm="24" style="text-align: center"></a-col>
+    </a-row>
+    <a-row style="margin-top: 30px;">
+      <a-col :span="24">
+        <current-objectives/>
+      </a-col>
+      <a-col :span="24">
+        <current-actions/>
+      </a-col>
+      <a-col :span="24">
+        <pending-auto-evaluations/>
+      </a-col>
+      <a-col :span="24">
+        <boss-evaluations/>
+      </a-col>
+    </a-row>
+    <video-modal-component ref="videoModal" :userRole="role"></video-modal-component>
+  </div>
 </template>
 
 <script>
 // *Utilities
 import loginDates from "@/services/loginDates";
 import videoPreference from "@/services/videoPreference";
+import authService from "@/services/auth";
 // *Components
 import nextEvaluationPeriod from "@/components/collaborator/home/nextEvaluationPeriod.vue";
 import collaboratorIssues from "@/components/collaborator/home/collaboratorIssues.vue";
@@ -53,8 +54,14 @@ export default {
         currentActions,
         videoModalComponent,
     },
+    data() {
+        return {
+            role: "",
+        };
+    },
     mounted() {
         if (!videoPreference.getShouldNeverDisplayVideo()) {
+            this.role = authService.getCurrentRole();
             const lastLoginDate = loginDates.getLastLoginDate();
             if (videoPreference.getVideoPreference() || loginDates.hasMoreThanOneDay(lastLoginDate)) {
                 this.$refs.videoModal.showModal();
