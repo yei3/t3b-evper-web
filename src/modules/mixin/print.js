a6d0a25d94420f2603c5a8c33ae0618378dd2805
luiarhs <luisangel@yei3.com>
2019-06-22 00:25:19 -0500

    [FIX] fix print evaluation

--- src/modules/mixin/print.js
+++ src/modules/mixin/print.js
@@ -34,6 +34,46 @@ export default {
                     console.error(err);
                 });
         },
+        $printEvaluation(el,
+            { 
+                name = "_blank",
+                replace = false,
+                specs = ["fullscreen=yes", "titlebar=no", "scrollbars=yes"],
+            } = {},
+        ) {
+            const element = document.getElementById(el);
+            if (!element) {
+                console.error(`Element "${el}" not found`);
+                return;
+            }
+
+            const WinPrint = window.open("", name, !specs.length ? specs.join(",") : "", replace);
+
+            let stylesHtml = "";
+            for (const node of [...document.querySelectorAll('link[rel="stylesheet"], style')]) {
+                // eslint-disable-line
+                stylesHtml += node.outerHTML;
+            }
+
+            WinPrint.document.write(`
+                <html>
+                    <head>
+                        <title>${document.title}</title>
+                        ${stylesHtml}
+                    </head>
+                    <body>
+                        ${element.innerHTML}
+                    </body>
+                </html>
+            `);
+
+            setTimeout(() => {
+                WinPrint.document.close();
+                WinPrint.focus();
+                WinPrint.print();
+                WinPrint.close();
+            }, 1000);
+        },
         ignoreElements(element) {
             return (
                 element.classList.contains("btn-blue") ||
