4f17e4dcbfe7c2c1bc2efd90b44ac5d6a65ebd93
sigfried <rssanjuan2704@gmail.com>
2019-09-15 23:26:28 -0500

    [MDF] User's admin component.

--- src/components/admin/users/users.vue
+++ src/components/admin/users/users.vue
@@ -1,118 +1,126 @@
-
 <template>
     <div class="users">
         <a-row class="breadcrumb-wrapper">
             <a-col :span="24">
-                <h1 class="breadcrumb-header">Administrador de usuarios</h1>
-            </a-col>
-            <a-col :span="24">
-                <a-breadcrumb>
-                    <a-breadcrumb-item>
-                        Modificar información de empleado
-                    </a-breadcrumb-item>
-                </a-breadcrumb>
+                <h1 class="breadcrumb-header">Administrar usuarios</h1>
             </a-col>
         </a-row>
         <div class="collapse-content">
-            <div>
-                <a-row class="steps">
-                    <span class="breadcrumb-header" style="font-weight: 400;">
-                        Búsqueda
-                    </span>
-                    <span style="font-size: 16px;">de usuarios</span>
-                </a-row>
-                <a-divider />
-                <a-row :gutter="20">
-                    <a-col :span="4">
-                        <a-select style="width: 100%" placeholder="Users">
-                            <a-select-option v-for="item in users" :key="item.id">
-                                {{ item.userName }}
-                            </a-select-option>
-                        </a-select>
-                    </a-col>
-                    <a-col :span="8">
-                        <a-select style="width: 100%" placeholder="Users">
-                            <a-select-option v-for="item in users" :key="item.id">
-                                {{ item.fullName }}
-                            </a-select-option>
-                        </a-select>
-                    </a-col>
-                    <a-col :span="8">
-                    </a-col>
-                    <a-col :span="4">
-                        <a-button type="primary" ghost @click="editUser()">
-                            Editar Usuario <a-icon type="edit" />
-                        </a-button>
-                    </a-col>
-                </a-row>
-            </div>
-            <a-divider />
-            <!-- Edit form -->
-            <div>
-                <a-row class="steps">
-                    <span class="breadcrumb-header" style="font-weight: 400;">
-                        Editar
-                    </span>
-                    <span style="font-size: 16px;">información de usuario</span>
-                </a-row>
-                <a-row class="">
-                    <a-form @submit="handleSubmit" :autoFormCreate="(form)=>{this.form = form}">
-                        <a-form-item
-                            v-bind="formItemLayout"
-                            label='E-mail'
-                        >
-                            <a-input
-                            v-decorator="[
-                                'email',
-                                {
-                                rules: [{
-                                    type: 'email', message: 'The input is not valid E-mail!',
-                                }, {
-                                    required: true, message: 'Please input your E-mail!',
-                                }]
+            <a-tabs defaultActiveKey="1">
+                <a-tab-pane key="1">
+                    <div slot="tab">
+                        <a-icon type="search" />Búsqueda y modificación de usuarios
+                    </div>
+
+                    <a-row class="steps">
+                        <span class="breadcrumb-header breadcrumb-header--fw-400">Búsqueda</span>
+                        <span style="font-size: 16px;">de usuarios</span>
+                    </a-row>
+                    <a-divider />
+                    <a-row :gutter="20">
+                        <a-col :span="4">
+                            <a-select style="width: 100%" placeholder="Users">
+                                <a-select-option v-for="item in users" :key="item.id">{{
+                                    item.userName
+                                }}</a-select-option>
+                            </a-select>
+                        </a-col>
+                        <a-col :span="8">
+                            <a-select style="width: 100%" placeholder="Users">
+                                <a-select-option v-for="item in users" :key="item.id">{{
+                                    item.fullName
+                                }}</a-select-option>
+                            </a-select>
+                        </a-col>
+                        <a-col :span="8"></a-col>
+                        <a-col :span="4">
+                            <a-button type="primary" :ghost="true" @click="editUser()">
+                                Editar Usuario
+                                <a-icon type="edit" />
+                            </a-button>
+                        </a-col>
+                    </a-row>
+                    <a-divider />
+                    <a-row class="steps">
+                        <span class="breadcrumb-header breadcrumb-header--fw-400">Editar</span>
+                        <span style="font-size: 16px;">información de usuario</span>
+                    </a-row>
+                    <a-row class>
+                        <a-form
+                            @submit="handleSubmit"
+                            :autoFormCreate="
+                                (form) => {
+                                    this.form = form;
                                 }
-                            ]"
-                            />
-                        </a-form-item>
-                        <a-form-item
-                            v-bind="formItemLayout"
-                            label='Escolaridad'
+                            "
                         >
-                            <a-input
-                            v-decorator="[
-                                'escolaridad',
-                                {
-                                rules: [{
-                                    type: 'text', message: 'The input is not valid!',
-                                }, {
-                                    required: true, message: 'Por favor llena este campo',
-                                }]
-                                }
-                            ]"
-                            />
-                        </a-form-item>
-                        <a-form-item v-bind="tailFormItemLayout">
-                            <a-button type='btn-green' htmlType='submit'>Guardar</a-button>
-                        </a-form-item>
-                    </a-form>
-                </a-row>
-            </div>
+                            <a-form-item v-bind="formItemLayout" label="E-mail">
+                                <a-input
+                                    v-decorator="[
+                                        'email',
+                                        {
+                                            rules: [
+                                                {
+                                                    type: 'email',
+                                                    message: 'The input is not valid E-mail!',
+                                                },
+                                                {
+                                                    required: true,
+                                                    message: 'Please input your E-mail!',
+                                                },
+                                            ],
+                                        },
+                                    ]"
+                                />
+                            </a-form-item>
+                            <a-form-item v-bind="formItemLayout" label="Escolaridad">
+                                <a-input
+                                    v-decorator="[
+                                        'escolaridad',
+                                        {
+                                            rules: [
+                                                {
+                                                    type: 'text',
+                                                    message: 'The input is not valid!',
+                                                },
+                                                {
+                                                    required: true,
+                                                    message: 'Por favor llena este campo',
+                                                },
+                                            ],
+                                        },
+                                    ]"
+                                />
+                            </a-form-item>
+                            <a-form-item v-bind="tailFormItemLayout">
+                                <a-button type="btn-green" htmlType="submit">Guardar</a-button>
+                            </a-form-item>
+                        </a-form>
+                    </a-row>
+                </a-tab-pane>
+                <a-tab-pane key="2">
+                    <div slot="tab"><a-icon type="upload" />Carga masiva</div>
+                    <upload-users />
+                </a-tab-pane>
+            </a-tabs>
         </div>
     </div>
 </template>
 <script>
 /* eslint-disable */
-import client3B from '@/api/client3B';
-import { mapActions, mapGetters } from 'vuex';
-import Footer from '@/components/layout/Footer.vue';
-import errorHandler from '@/views/errorHandler';
+import client3B from "@/api/client3B";
+import { mapActions, mapGetters } from "vuex";
+import Footer from "@/components/layout/Footer.vue";
+import errorHandler from "@/views/errorHandler";
+import successHandler from "@/views/successHandler";
 
 export default {
     components: {
         Footer,
+        "upload-users": () => import("../../shared/uploadUsers"),
     },
-    beforeCreate () {
-        this.form = this.$form.createForm(this)
+    beforeCreate() {
+        this.form = this.$form.createForm(this);
     },
     data() {
         return {
@@ -143,25 +151,28 @@ export default {
         update() {
             this.loading = true;
         },
-        onSearch (value) {
-            console.log(value)
+        onSearch(value) {
+            console.log(value);
         },
-        handleSubmit  (e) {
-            e.preventDefault()
+        handleSubmit(e) {
+            e.preventDefault();
             this.form.validateFieldsAndScroll((err, values) => {
                 if (!err) {
-                    console.log('Received values of form: ', values)
+                    console.log("Received values of form: ", values);
                 }
                 console.log(values);
-            })
+            });
         },
     },
 };
 </script>
 
 <style>
-    .collapse-content {
-        margin: 32px;
-        background: white;
-    }
+.collapse-content {
+    margin: 32px;
+    background: white;
+}
+.breadcrumb-header--fw-400 {
+    font-weight: 400;
+}
 </style>
