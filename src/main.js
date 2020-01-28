6ae9df7579311188c2894c8a50e2f4997c9c36b4
sigfried <rssanjuan2704@gmail.com>
2020-01-27 21:08:57 -0600

    [ADD] Set es_ES locale to application.

--- src/main.js
+++ src/main.js
@@ -1,14 +1,11 @@
 import Vue from "vue";
 import Antd from "ant-design-vue";
-import moment from "moment";
 import "ant-design-vue/dist/antd.css";
 import "@/assets/styles/general.css";
 
 import App from "./App.vue";
 import router from "./routes/router";
 import store from "./store/store";
-
-moment.locale("es");
 
 Vue.config.productionTip = false;
 Vue.use(Antd);
