4d0c0a80c400b1728735cc4af3e9f0b1f53507b5
sigfried <rssanjuan2704@gmail.com>
2020-02-20 08:41:31 -0600

    [ADD] Constants file for roles & date format.

---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+export const DATE_FORMAT = "D/M/YY HH:mm:ss A";
+
+export const ROLES = Object.freeze({
+    ADMIN: "Admin",
+    ADMINISTRATOR: "Administrator",
+    SUPERVISOR: "Supervisor",
+    COLLABORATOR: "Collaborator",
+});
