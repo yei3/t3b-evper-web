a18c9f1c9148bb17c9d178e06cef3bc68636bee9
sigfried <rssanjuan2704@gmail.com>
2020-02-29 13:05:07 -0600

    [REF] Decouple mutations & actions from User state.

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,4 +1,5 @@
-import client3B from "@/api/client3B";
+import ACTIONS from "./actions";
+import MUTATIONS from "./mutations";
 
 export default {
     state: {
@@ -10,46 +11,9 @@ export default {
         userAreaCode: ({ userData }) => userData.areaCode,
     },
     mutations: {
-        setUserData: (_state, userData) => {
-            const state = _state;
-            state.userData = userData;
-        },
-        evaluationSetQuestions: (_state, questions) => {
-            const state = _state;
-            state.questions = questions;
-        },
-        evaluationAddQuestionStatus: (_state, question) => {
-            const state = _state;
-            state.questions.push(question);
-        },
-        evaluationSetQuestionsAsAnswered: (_state, questionId) => {
-            const state = _state;
-            const question = state.questions.find((qst) => qst.id === questionId);
-            question.answered = true;
-        },
-        updateRegionCode(_state, newUserRegionCode) {
-            const state = _state;
-            state.userData.regionCode = newUserRegionCode;
-        },
-        updateAreaCode(_state, newUserAreaCode) {
-            const state = _state;
-            state.userData.areaCode = newUserAreaCode;
-        },
+        ...MUTATIONS.userMutations,
     },
     actions: {
-        getUserAsync: async ({ commit }, userId) => {
-            try {
-                commit("requestStart");
-
-                const { data } = await client3B.user.getUserByUserName(userId);
-                const { result } = data;
-
-                commit("setUserData", result);
-            } catch (error) {
-                commit("requestError", { errors: error.message });
-            } finally {
-                commit("requestEnd");
-            }
-        },
+        ...ACTIONS.userActions,
     },
 };
