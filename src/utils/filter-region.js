9a806953d64952a12c6fbbaa87d309b8b7d4393d
sigfried <rssanjuan2704@gmail.com>
2020-02-29 12:57:33 -0600

    [REF] Filter region docs & fix tests.

--- src/utils/filter-region.js
+++ src/utils/filter-region.js
@@ -1,7 +1,23 @@
+/**
+ * Returns the region given the user's region code
+ *
+ * @typedef {Region} - Region Object shape
+ * @prop {string} - displayName
+ * @prop {string} - code
+ * @prop {string} - parentId
+ * @prop {array} - organizationUnitUsers
+ * @prop {boolean} - isSalesArea
+ * @prop {number} - id
+ *
+ * @param {string} userRegion - User's region code
+ * @param {Region[]} regions - Array of region Objects
+ *
+ * @return {Region}
+ */
 const searchRegion = (userRegion, regions) => {
     const [resultRegion] = regions.filter((region) => region.code === userRegion);
 
-    return resultRegion.code;
+    return resultRegion;
 };
 
 export default searchRegion;
