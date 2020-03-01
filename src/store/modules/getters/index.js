d7c77c1e5da15541a0454746745ef9e4e8a95356
sigfried <rssanjuan2704@gmail.com>
2020-03-01 13:59:47 -0600

    [ADD] Evaluation getter default export.

--- src/store/modules/getters/index.js
+++ src/store/modules/getters/index.js
@@ -1,4 +1,5 @@
 import userGetters from "./userGetters";
 import transientGetters from "./transientGetters";
+import evaluationGetters from "./evaluationGetters";
 
-export default { userGetters, transientGetters };
+export default { userGetters, transientGetters, evaluationGetters };
