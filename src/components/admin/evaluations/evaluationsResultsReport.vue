ddfe91eea9b2f98d2ee7af1c7782114b499bd1da
sigfried <rssanjuan2704@gmail.com>
2020-04-01 12:01:20 -0600

    [ADD] Initla EvaluationsResultsReport definition.

---
+++ src/components/admin/evaluations/evaluationsResultsReport.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="collapse-content" style="background-color: white; margin: 30px 30px;">
+        <h3 class="breadcrumb-header">Reportes de resultados de evaluaciones</h3>
+        <a-divider />
+        <a-row type="flex" justify="center" :gutter="8">
+            <a-col :span="8">
+                <a-range-picker
+                    :showTime="true"
+                    :placeholder="pickerPlaceholders"
+                    :format="RANGE_PICKER_FORMAT"
+                />
+            </a-col>
+            <a-col>
+                <a-button class="ml-2" :loading="false">
+                    <a-icon type="file" />
+                    <span>Generar Excel con resultados</span>
+                </a-button>
+            </a-col>
+            <a-button class="ml-2" type="primary" icon="download">Descargar Excel</a-button>
+        </a-row>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            pickerPlaceholders: ["Fecha inicial", "Fecha final"],
+            RANGE_PICKER_FORMAT: "YYYY-DD-MM, HH:mm:ss",
+        };
+    },
+};
+</script>
+
+<styles scoped>
+</styles>
