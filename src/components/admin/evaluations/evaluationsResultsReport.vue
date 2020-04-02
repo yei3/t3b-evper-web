a3b1058bd933b411a2ad815e896b7b5bcc90751e
sigfried <rssanjuan2704@gmail.com>
2020-04-02 11:10:49 -0600

    [REF] Evaluation Results view methods.

--- src/components/admin/evaluations/evaluationsResultsReport.vue
+++ src/components/admin/evaluations/evaluationsResultsReport.vue
@@ -8,19 +8,39 @@
                     :showTime="true"
                     :placeholder="pickerPlaceholders"
                     :format="RANGE_PICKER_FORMAT"
+                    @change="onRangePickerChange"
+                    @ok="handleOnOK"
                 />
             </a-col>
             <a-col>
-                <a-button class="ml-2" :loading="false">
+                <a-button
+                    class="ml-2"
+                    :loading="false"
+                    @click="generateFile"
+                    :disabled="isButtonDisabled"
+                >
                     <a-icon type="file" />
                     <span>Generar Excel con resultados</span>
                 </a-button>
             </a-col>
-            <a-button class="ml-2" type="primary" icon="download">Descargar Excel</a-button>
+            <a-button
+                class="ml-2"
+                type="primary"
+                icon="download"
+                v-show="generatedURL !== ''"
+                :href="generatedURL"
+                >Descargar Excel</a-button
+            >
         </a-row>
         <a-row class="main-content" style="margin-top: 30px;">
             <a-col :span="24" style="padding-bottom: 30px;">
-                <a-table :dataSource="data" :columns="columns">
+                <a-table
+                    :dataSource="data"
+                    :columns="columns"
+                    :pagination="pagination"
+                    :loading="loading"
+                    :rowKey="generateRowKey"
+                >
                     <div class="collaborator" slot="collaborator" slot-scope="text, record">
                         <p class="collaborator__main-info">
                             {{ record.employeeNumber }}
@@ -74,6 +94,9 @@
 <script>
 import moment from "moment";
 import client3B from "@/api/client3B";
+import errorHandler from "@/views/errorHandler";
+import successHandler from "@/views/successHandler";
+import { mapMutations, mapGetters } from "vuex";
 import evaluationResultColumns from "./constants/resultColumns";
 
 export default {
@@ -90,15 +113,16 @@ export default {
                 initDate: "",
                 endDate: "",
             },
+            generatedURL: "",
             pickerPlaceholders: ["Fecha inicial", "Fecha final"],
             RANGE_PICKER_FORMAT: "YYYY-DD-MM, HH:mm:ss",
             columns: evaluationResultColumns,
             data: [],
-            loading: false,
         };
     },
     methods: {
         async handlePaginationChange(page, pageSize) {
+            this.requestStart();
             const skipCount = (page - 1) * pageSize;
             const pagination = { SkipCount: skipCount };
             let serverResponse;
@@ -117,15 +141,15 @@ export default {
                 const { items } = result;
                 this.data = items;
             } catch (error) {
-                console.log(error);
-                // errorHandler(this, error);
+                this.requestError(error.message);
+                errorHandler(this, error);
             } finally {
-                this.loading = false;
+                this.requestEnd();
             }
         },
         async getEvaluationsReportList() {
             try {
-                this.loading = true;
+                this.requestStart();
                 const initDate = moment(new Date()).toISOString();
                 const endDate = moment(initDate)
                     .add(1, "day")
@@ -136,12 +160,73 @@ export default {
                 this.data = items;
                 this.pagination.total = totalCount;
             } catch (error) {
-                console.log(error);
-                // errorHandler(this, error);
+                this.requestError(error.message);
+                errorHandler(this, error);
             } finally {
-                this.loading = false;
+                this.requestEnd();
             }
         },
+        generateRowKey(record) {
+            return record.id;
+        },
+        onRangePickerChange(date, dateString) {
+            const [initDate, endDate] = date;
+            const [formattedInitDate, formattedEndDate] = dateString;
+            if (formattedInitDate === "" && formattedEndDate === "") {
+                this.filters.initDate = formattedInitDate;
+                this.filters.endDate = formattedInitDate;
+                this.generatedURL = "";
+                this.getEvaluationsList();
+            } else {
+                this.filters.initDate = new Date(initDate).toISOString();
+                this.filters.endDate = new Date(endDate).toISOString();
+            }
+        },
+        async getEvaluationWithFilter() {
+            try {
+                this.requestStart();
+                const { data } = await client3B.evaluation.getEvaluationsResult(
+                    this.filters.initDate,
+                    this.filters.endDate,
+                );
+                const { result } = data;
+                const { items, totalCount } = result;
+                this.data = items;
+                this.pagination.total = totalCount;
+            } catch (error) {
+                this.requestError(error.message);
+                errorHandler(this, error);
+            } finally {
+                this.requestEnd();
+            }
+        },
+        handleOnOK() {
+            this.getEvaluationWithFilter();
+        },
+        async generateFile() {
+            try {
+                this.isGeneratingFile = true;
+                const { data } = await client3B.evaluation.generateEvaluationsResultFile({
+                    StartDateTime: this.filters.initDate,
+                    EndDateTime: this.filters.endDate,
+                });
+                const { result } = data;
+                const { fileName, fileType, fileToken } = result;
+                this.generatedURL = `${process.env.VUE_APP_API_URL}/api/File/DownloadTempFile?fileName=${fileName}&fileType=${fileType}&fileToken=${fileToken}`;
+                successHandler(this, "Archivo generado correctamente");
+            } catch (error) {
+                errorHandler(this, error);
+            } finally {
+                this.isGeneratingFile = false;
+            }
+        },
+        isButtonDisabled() {
+            return this.filters.initDate !== "" && this.filters.endDate !== "";
+        },
+        ...mapMutations(["requestStart", "requestEnd", "requestError"]),
+    },
+    computed: {
+        ...mapGetters(["loading", "errors"]),
     },
 };
 </script>
